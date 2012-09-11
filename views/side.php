5488c64c15a86a45935152a89cba43a0c2281677
Eric Arenson <eric.arenson@portlandoregon.gov>
2012-09-11 16:47:22 -0700

    Working out XML parsing

--- views/side.php
+++ views/side.php
@@ -1,16 +1,53 @@
 <?php 
 
+// Init some vars
+$days = 5;
 
 // Pulling some data from NOAA API
-$xmlDoc = simplexml_load_file('http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?lat=45.51&lon=-122.68&format=12+hourly&numDays=5');
+//$xmlDoc = simplexml_load_file('http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?lat=45.51&lon=-122.68&format=12+hourly&numDays='.$days);
+
+$xmlDoc = simplexml_load_file('weather-gov.xml'); // Sample file for testing
+
+$weatherData = $xmlDoc->data;
+
+// the XML nodes from NOAA have hyphens in the node names, which trip up the parser. They need 
+// to be accessed as {'node-name'}
+
+/*
+echo '<pre>';
+print_r($weatherData);
+echo '</pre>';
+*/
+
+// Build weather array from XML feed
+$i = 0;
+$weatherArray = array();
+
+
+/*
+foreach($weatherData->time-layout->start-valid-time as $time){
+
+    echo $time
+
+}
+*/
+
+
+while($i<=$days-1){
+    $weatherArray[$i] = array(
+        'time_start' => (string)$weatherData->{'time-layout'}->{'start-valid-time'}[$i][0],
+        'time_end' => (string)$weatherData->{'time-layout'}->{'end-valid-time'}[$i][0],
+        'max_temp' => (string)$weatherData->parameters->temperature[0]->value[$i],
+        'min_temp' => (string)$weatherData->parameters->temperature[1]->value[$i]
+    );
+    
+    $i++;
+    
+}
 
 echo '<pre>';
-print_r($xmlDoc);
+print_r($weatherArray);
 echo '</pre>';
-
-
-$weather = $xmlDoc->data->parameters->weather;
-//print_r($weather->name);
 
 // var_dump($xmlDoc->data->parameters->weather)
 
@@ -22,17 +59,7 @@ $weather = $xmlDoc->data->parameters->weather;
     <div class="current">
         <?php
 
-            $temp = $xmlDoc->data->parameters->temperature->name;
-            $conditions = $xmlDoc->data->parameters->weather->{'weather-conditions'};
-
-            $img = $weather[0];
-    
-            foreach($conditions AS $condition) {
-                $attr = $condition->attributes();
-                echo $attr['weather-summary'] , '<br />';
-            }
-
-            echo $img;
+            
         
         ?>
     </div>
