6e62fcba8e35bf76b47fe09f9ee82f9b6c999a9e
Rob Alan <rob@robalan.com>
2012-09-11 14:05:11 -0700

    Modified the xml parsing in prep for layout and styling

--- views/side.php
+++ views/side.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-// Pulling some data from Yahoo! Weather API
+// Pulling some data from NOAA API
 $xmlDoc = simplexml_load_file('http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?lat=45.51&lon=-122.68&format=12+hourly&numDays=5');
 
 echo '<pre>';
@@ -12,38 +12,31 @@ echo '</pre>';
 $weather = $xmlDoc->data->parameters->weather;
 //print_r($weather->name);
 
-?>
+// var_dump($xmlDoc->data->parameters->weather)
 
-
-<?php 
-
-// Get current weather from Weather.gov
-$current = simplexml_load_file('http://www.weather.gov/xml/current_obs/KHIO.rss');
-echo '<pre>';
-print_r($current);
-echo '</pre>';
 
 ?>
-
-
 
 
 <div class="widget-side" style="width:220px;">
     <div class="current">
         <?php
-            
-            $desc = $current->channel->item->description;
-            
-            $desc = explode("<br>",$desc);
-            $text = $desc[1];
-            $img = $desc[0];
+
+            $temp = $xmlDoc->data->parameters->temperature->name;
+            $conditions = $xmlDoc->data->parameters->weather->{'weather-conditions'};
+
+            $img = $weather[0];
     
+            foreach($conditions AS $condition) {
+                $attr = $condition->attributes();
+                echo $attr['weather-summary'] , '<br />';
+            }
+
             echo $img;
-            // NOTE: This isn't working;
         
         ?>
     </div>
-<!--     <h2 class="next">Tomorrow:</h2> -->
+    <h2 class="next">Tomorrow:</h2>
 </div>
 
 <script type="text/javascript">
