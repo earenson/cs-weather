a1bc8b994b08a35685a9d01e57799a2d1e432a88
Rob Alan <robalan@macmini.rose.portland.local>
2012-09-19 14:03:30 -0700

    Initial push, app with hard-coded data, tabs, but no 'slide down' method

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
 <head>
     <link href="http://pdxcitysync.org/apps/citysync_styles.css" rel="stylesheet" type="text/css">
 </head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+    <script src="libs/js/jquery-ui-1.8.23.custom.min.js"></script>
     <script src="http://pdxcitysync.org/apps/citysync-app.js" type="text/javascript"></script>
 
 <body>
     <?php
 
     /**
-     * CitySync App: Weather
+     * CitySync App: Local Events
      * Author: Eric Arenson
-     * Version: 1.2
+     * Version: 1.1
      */
 
         $region = $_GET['region'];  // The context in which the app is being displayed
